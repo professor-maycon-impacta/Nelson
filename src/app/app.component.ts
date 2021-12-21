86dfe230a534b79681185b3697653550e43cbc3f
Lima <nelson.lima@capgemini.com>
2021-12-21 12:37:37 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MinhaAppAngular';
+
+  ngOnint(){
+alert("Meu curriculo")
+  }
+}
